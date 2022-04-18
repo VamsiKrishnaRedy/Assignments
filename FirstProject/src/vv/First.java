4901e5039cc856887b2de0204d913acac643f9ec
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-18 16:35:45 +0530

    Odd numbers from 1 to 100

--- FirstProject/src/vv/First.java
+++ FirstProject/src/vv/First.java
@@ -3,24 +3,18 @@ package vv;
 public class First {
 
 	public static void main(String[] args) {
+		int i=0;
+		System.out.println("The odd numbers between 1 to 100");
+		while(i<100) {
+			if(i%2!=0) {
+				System.out.println(i);
+				}
+			
+			i++;
+		}
 		
-		int res=add(10,20);
-		System.out.println("The sum if two numbers is :"+res);
-		StudentInfo s1=new StudentInfo();
-		s1.name="Vamsi Krishna Reddy";
-		s1.age=21;
-		s1.address="Hyderabad";
-		System.out.println(s1.name);
-		System.out.println(s1.address);	
-		System.out.println(s1.age);
-		
-				
 		
 		
 	}
- static int add(int a,int b) {
-	 int result=a+b;
-	 return result;
-	 
- }
+ 
 }
