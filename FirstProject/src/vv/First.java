ee6445ed46d4f54bbf475c17c96dfcb838d62ac7
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-19 14:39:32 +0530

    Inheritance

--- FirstProject/src/vv/First.java
+++ FirstProject/src/vv/First.java
@@ -2,19 +2,40 @@ package vv;
 
 import java.util.Scanner;
 
-public class First {
 
-	public static void main(String[] args) {
-
-		
-		System.out.println(StudentInfo.FindPalindrome(10));
-		
-
-		
-
-
-
+class Animal{
+	void run() {
+		System.out.println("Running...");
 	}
-	
 
 }
+
+class Dog extends Animal{
+	void bark() {
+		System.out.println("Barking...");
+	}
+}
+
+class cat extends Dog{
+		void meow() {
+			System.out.println("Cat is drinking...");
+		}
+	}
+
+
+
+ class First  {
+
+
+		public static void main(String[] args) {
+			//cat c1=new cat();
+			StudentInfo s1=new StudentInfo();	
+			s1.bark();
+			s1.run();
+			s1.meow();
+			s1.play();
+			
+
+		}
+
+	}
--- FirstProject/src/vv/StudentInfo.java
+++ FirstProject/src/vv/StudentInfo.java
@@ -1,22 +1,11 @@
 package vv;
 
-public class StudentInfo {
+ class StudentInfo extends cat {
 	
 	
-	static int FindPalindrome(int a) {
-		int n1=0;
-		int n2=1;
-		int n3=0;
-		for(int i=0;i<a;i++) {
-			n3=n1+n2;
-			System.out.println(n1);
-			n1=n2;
-			n2=n3;
-			
-			
-			
-		}
-		return 0;
+	void play() {
+		System.out.println("Playing...");
+		
 		
 	}
 	
