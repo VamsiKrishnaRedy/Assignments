81b0fe7de5825e3fbb8fa0bb03500c8afd108d0d
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-19 11:07:56 +0530

    Methods

--- FirstProject/src/vv/First.java
+++ FirstProject/src/vv/First.java
@@ -1,20 +1,31 @@
 package vv;
 
+import java.util.Scanner;
+
 public class First {
 
 	public static void main(String[] args) {
-		int i=0;
-		System.out.println("The odd numbers between 1 to 100");
-		while(i<100) {
-			if(i%2!=0) {
-				System.out.println(i);
-				}
-			
-			i++;
-		}
+
 		
-		
-		
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num=s1.nextInt();
+		factorial(num);
+
+
+
+
 	}
- 
+	public static void factorial(int a) {
+		int fact=1;
+		int i=1;
+		while(i<=a) {
+			fact=fact*i;
+			i++;
+
+		}
+		System.out.println("The factorial of  "+a+" is :"+fact);
+
+	}
+
 }
