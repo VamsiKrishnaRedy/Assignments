009e1eb1f89caed20930c9e1396c33195d49d00b
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-20 10:25:38 +0530

    Method Overloading

--- FirstProject/src/vv/First.java
+++ FirstProject/src/vv/First.java
@@ -1,24 +1,14 @@
 package vv;
 
 import java.util.Scanner;
-class area{
-	StudentInfo st1;
-	double findarea(int r) {
-		st1=new StudentInfo();
-		int res1=st1.square(r);
-		return 3.14*res1;
-		
-		} 
-}
+
 
 class First  {
 	
-	
-
 	public static void main(String[] args) {
-		area a1=new area();
-		double d1=a1.findarea(10);
-		System.out.println(d1);
+		System.out.println(StudentInfo.add(10,20));
+		
+		System.out.println(StudentInfo.add(10,20,30));
 		
 		
 		
