3e25f08173eade8fc31794f2770fef598ed6cb1e
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-26 11:54:20 +0530

    Java LinkedList

--- FirstProject/src/vv/First.java
+++ FirstProject/src/vv/First.java
@@ -1,16 +1,24 @@
 package vv;
 
-
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 class First {
 
-       
 	    public static void main(String[] args) {
-	   
-		
-
-		
+	 LinkedList<String> l1=new LinkedList<>();
+	 l1.add("Vamsi");
+	 l1.add("Krish");
+	 l1.add("reddy");
+	 l1.add("Sanju");
+	 l1.add("Sai");
+	 Iterator<String> itr= l1.iterator();
+	 while(itr.hasNext()) {
+		 System.out.println(itr.next());
+	 }
+	 
 }
 }
 
