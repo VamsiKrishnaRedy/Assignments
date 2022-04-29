4fc9b776b5363ddee1c69f7f83d883d5dfa7839a
VVemireddy <VVemireddy@INBAWN166593.ad.harman.com>
2022-04-29 10:07:27 +0530

    Factory Design Patterns

---
+++ FirstProject/src/designpatterns/CommercialPlan.java
@@ -0,0 +1,7 @@
+package designpatterns;
+class  CommercialPlan extends Plan{  
+	//@override   
+	public void getRate(){   
+		rate=7.50;  
+	}   
+} 
---
+++ FirstProject/src/designpatterns/DomesticPlan.java
@@ -0,0 +1,7 @@
+package designpatterns;
+class  DomesticPlan extends Plan{  
+	//@override  
+	public void getRate(){  
+		rate=3.50;              
+	}  
+}//end o
